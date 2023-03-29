c58ede5ac4c21378eebbc89b522c14753308ed13
chinmaya.patel <chinmaya.patel@skyspecs.com>
2023-03-29 23:22:16 +0530

    Tested Link tags for routing and Fast Refresh

---
+++ front/pages/about.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+function About() {
+  return (
+    <>
+      <h1>
+        About
+      </h1>
+      <Link href="/">Home</Link>
+    </>
+  )
+}
+
+export default About
---
+++ front/pages/blog/hello-world.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+function Hello() {
+  return (
+    <>
+        <h1>
+            Hello World
+        </h1>
+        <Link href="/">Home</Link>
+    </>
+  )
+}
+
+export default Hello
